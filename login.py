3f9b599aea416b3e3949a2f7532ccdb27db374ca
aline-bebe <ingabirealine40@gmail.com>
2019-09-05 11:10:11 +0200

    starting my project by creating files

---
+++ login.py
@@ -0,0 +1,18 @@
+class Locker:
+    """
+    Class that generates new instances of password 
+    """
+
+    def __init__(self,first_name,last_name,password,email):
+
+        '''
+        __init__ method that helps us define properties for our objects.
+
+        Args:
+            first_name: New locker first name.
+            last_name : New locker last name.
+            number: New locker password.
+            email : New locker email address.
+        '''
+
+    pass
